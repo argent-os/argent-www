aa2407e19558782e585fe79f62597f8d16eb709c
Sinan Ulkuatam <sulkuatam@gmail.com>
2016-02-26 03:34:12 -0500

    add paykloud api endpoint

---
+++ src/app/index.js
@@ -0,0 +1,85 @@
+// (function() {
+//     'use strict';
+//     // THIS FILE IS IS IGNORED BY .ebignore AND .gitignore TO PREVENT ACCIDENTAL CHANGES TO API ROOT URL CONFIG IN PRODUCTION
+//     var app = angular.module('app', [
+//         'app.entity'
+//     ])
+//     // DEFAULT USED AT LOGIN ** EDIT THIS BEFORE RELEASING PRODUCTION
+//     .value('appconfig',{
+//             apiRoot: 'http://localhost:5001',
+//             firebaseUrl: 'https://demosandbox.firebaseio.com/api/v1'
+//     })
+//     .config(['stripeProvider', 'notificationsConfigProvider', function (stripeProvider, notificationsConfigProvider) {
+//         // ***CHANGE STRIPE KEY BEFORE RELEASING PRODUCTIONS***
+//         // Prod Stripe Pub Key
+//         // stripeProvider.setPublishableKey('pk_live_9kfmn7pMRPKAYSpcf1Fmn266');
+//         // Dev Stripe Pub Key
+//         stripeProvider.setPublishableKey('pk_test_6MOTlPN5JrNS5dIN4DUeKFDA');
+//         // auto hide
+//         notificationsConfigProvider.setAutoHide(true);
+
+//         // delay before hide
+//         notificationsConfigProvider.setHideDelay(3000);
+
+//         // support HTML
+//         notificationsConfigProvider.setAcceptHTML(false);
+   
+//     }])
+//     .run(['$http', '$rootScope','appconfig', 'UserFactory', function($http, $rootScope, appconfig, UserFactory) {
+//         UserFactory.getProfile().then(function (res) {
+//             if(res.data.env == 'DEV') {
+//                 appconfig.apiRoot = 'http://localhost:5001';
+//                 appconfig.firebaseUrl = 'https://demosandbox.firebaseio.com/api/v1'
+//             } else if(res.data.env == 'PROD') {
+//                 appconfig.apiRoot = 'http://paykloud-api-dev.us-east-1.elasticbeanstalk.com';
+//                 appconfig.firebaseUrl = 'https://paykloud.firebaseio.com/api/v1'
+//             }
+//         })
+//     }])
+
+
+// })();
+
+(function() {
+    'use strict';
+    // THIS FILE IS IS IGNORED BY .ebignore AND .gitignore TO PREVENT ACCIDENTAL CHANGES TO API ROOT URL CONFIG IN PRODUCTION
+    var app = angular.module('app', [
+        'app.entity'
+    ])
+    // DEFAULT USED AT LOGIN ** EDIT THIS BEFORE RELEASING PRODUCTION
+    // API endpoints
+    // http://api.paykloud.com
+    // http://dev.paykloud.com
+    // http://paykloud-api-dev.us-east-1.elasticbeanstalk.com
+    .value('appconfig',{
+            apiRoot: 'http://paykloud-api-dev.us-east-1.elasticbeanstalk.com',
+            firebaseUrl: 'https://paykloud.firebaseio.com/api/v1'
+    })
+    .config(['stripeProvider', 'notificationsConfigProvider', function (stripeProvider, notificationsConfigProvider) {
+        // ***CHANGE STRIPE KEY BEFORE RELEASING PRODUCTIONS***
+        // Prod Stripe Pub Key
+        stripeProvider.setPublishableKey('pk_live_9kfmn7pMRPKAYSpcf1Fmn266');
+        // auto hide
+        notificationsConfigProvider.setAutoHide(true);
+
+        // delay before hide
+        notificationsConfigProvider.setHideDelay(3000);
+
+        // support HTML
+        notificationsConfigProvider.setAcceptHTML(false);
+   
+    }])
+    .run(['$http', '$rootScope','appconfig', 'UserFactory', function($http, $rootScope, appconfig, UserFactory) {
+        UserFactory.getProfile().then(function (res) {
+            if(res.data.env == 'DEV') {
+                appconfig.apiRoot = 'http://localhost:5001';
+                appconfig.firebaseUrl = 'https://demosandbox.firebaseio.com/api/v1'
+            } else if(res.data.env == 'PROD') {
+                appconfig.apiRoot = 'http://paykloud-api-dev.us-east-1.elasticbeanstalk.com';
+                appconfig.firebaseUrl = 'https://paykloud.firebaseio.com/api/v1'
+            }
+        })
+    }])
+
+
+})();
