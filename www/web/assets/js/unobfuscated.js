2e9d609353c2d030dc0bd483e3f681bb714705b3
Sinan Ulkuatam <sulkuatam@gmail.com>
2016-06-07 11:21:34 -0400

    add twilio integration for app text, edit config.yml and project.config wip

---
+++ www/web/assets/js/unobfuscated.js
@@ -0,0 +1,34 @@
+function sendMessage() {
+    //console.log("submitting")
+    var form = $('#phoneNumberForm');
+    var formattedPhoneNumber = $('input[name="PhoneNumber"]').val()
+    var rawPhoneNumber = formattedPhoneNumber.replace(/\D/g,'');
+    var phoneNumberUS = "+1"+rawPhoneNumber
+    // console.log("phone number is", "+1" + rawPhoneNumber);
+    // console.log(form)
+    $.ajax({
+        type: "POST",
+        url: "https://api.argent.cloud/v1/twilio",
+        dataType: "json",  
+        contentType: 'application/json; charset=UTF-8', // This is the money shot
+        data: JSON.stringify({
+            "phone": phoneNumberUS
+        }),
+        success: function (data) {
+            swal({   
+                title: "Success!",   
+                text: "App link is on it's way",   
+                type: "success",   
+                confirmButtonText: "Sweet" 
+            });
+        },
+        failure: function(err) {
+            swal({   
+                title: "Error",   
+                text: "App could not be sent to phone :(",   
+                type: "error",   
+                confirmButtonText: "Ok" 
+            });
+        }
+    });
+}
